b9c07f183600b849961b39d2847fec58a3204d18
Vasco Lopes <vascocajada@gmail.com>
2023-06-30 17:34:59 +0100

    rector config

--- rector.php
+++ rector.php
@@ -3,15 +3,14 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Set\ValueObject\SymfonyLevelSetList;
-use Rector\Set\ValueObject\SymfonySetList;
+use Rector\Symfony\Set\SymfonyLevelSetList;
+use Rector\Symfony\Set\SymfonySetList;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->symfonyContainerXml(__DIR__ . '/var/cache/dev/App_KernelDevDebugContainer.xml');
 
     $rectorConfig->sets([
         SymfonyLevelSetList::UP_TO_SYMFONY_54,
-        SymfonySetList::SYMFONY_62,
         SymfonySetList::SYMFONY_CODE_QUALITY,
         SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION,
     ]);
